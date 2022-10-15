8e040c33158ac78b7e434e8c2f2daecba00d5d0f
SAI <86056964+KSAIII@users.noreply.github.com>
2022-10-15 19:31:58 +0530

    Add files via upload

---
+++ emoji changer/index.js
@@ -0,0 +1,15 @@
+const closedFace = document.querySelector('.closed')
+const openFace = document.querySelector('.open')
+
+closedFace.addEventListener('click',()=> {
+    if(openFace.classList.contains('open')) {
+        openFace.classList.add('active');
+        closedFace.classList.remove('active');
+    }
+});
+openFace.addEventListener('click',()=>{
+    if(closedFace.classList.contains('closed')){
+closedFace.classList.add('active');
+openFace.classList.remove('active');
+    }
+}) ;
